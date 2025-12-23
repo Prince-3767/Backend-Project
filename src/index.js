06c42b83bfdbb04990fe4f4850ba0e43497b1834
Prince-3767 <pbar798@gmail.com>
2025-12-23 18:03:41 +0530

    Custom Api errors

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({});
+import { app } from "./app.js";
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+    app.on("error", (error) => {
+      console.log("App error: ", error);
+      throw error;
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MONGODB Connection Failed: ${err}`);
+  });
+
+// (async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     } catch (error) {
+//         console.log("Error: ",error);
+//         throw error
+
+//     }
+// })()
